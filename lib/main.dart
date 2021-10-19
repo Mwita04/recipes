5a8a447efab0ab9e285180b82873a53c81da66cf
Mwita04 <emcee779@gmail.com>
2021-10-19 22:47:24 +0300

    commit on master

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,36 @@
 // ignore_for_file: use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+import 'recipe.dart';
 
 void main() {
   runApp(RecipeApp());
 }
 
 class RecipeApp extends StatelessWidget {
- // const RecipeApp({Key? key}) : super(key: key);
+  // const RecipeApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application
   // 1
-@override
-Widget build(BuildContext context) {
-  // 2
-  final ThemeData theme = ThemeData();
-  // 3
-  return MaterialApp(
-    // 4
-    title: 'Recipe Calculator',
-    // 5
-    theme: theme.copyWith(
+  @override
+  Widget build(BuildContext context) {
+    // 2
+    final ThemeData theme = ThemeData();
+    // 3
+    return MaterialApp(
+      // 4
+      title: 'Recipe Calculator',
+      // 5
+      theme: theme.copyWith(
         colorScheme: theme.colorScheme.copyWith(
-            primary: Colors.grey,
-            secondary: Colors.black,
+          primary: Colors.grey,
+          secondary: Colors.black,
         ),
-    ),
-    // 6
-    home: const MyHomePage(title: 'Recipe Calculator'),
-  );
-}
+      ),
+      // 6
+      home: const MyHomePage(title: 'Recipe Calculator'),
+    );
+  }
 }
 
 class MyHomePage extends StatefulWidget {
@@ -42,6 +43,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  //
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      // 1
+      elevation: 2.0,
+      // 2
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      // 3
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        // 4
+        child: Column(
+          children: <Widget>[
+            Image(image: AssetImage(recipe.imageUrl)),
+            // 5
+            const SizedBox(
+              height: 14.0,
+            ),
+            // 6
+            Text(
+              recipe.label,
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Palatino',
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // 1
@@ -52,12 +86,21 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       // 3
       body: SafeArea(
-        // TODO: Replace child: Container()
         // 4
-        child: Container(),
+        child: ListView.builder(
+          // 5
+          itemCount: Recipe.samples.length,
+          // 6
+          itemBuilder: (BuildContext context, int index) {
+            // 7
+            //
+            // TODO: Add GestureDetector
+            return buildRecipeCard(Recipe.samples[index]);
+
+            return Text(Recipe.samples[index].label);
+          },
         ),
+      ),
     );
   }
-
-  // TODO: Add buildRecipeCard() here
 }
